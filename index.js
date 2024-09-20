baf1794bd512c7520b899612e4b374a63a59ea8a
Will Olson <will.olson.b@gmail.com>
2024-09-20 11:59:52 -0500

    Nondestructive array methods added to index.js to resolve failing tests.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  appendCat,
+  destructivelyPrependCat,
+  prependCat,
+  destructivelyRemoveLastCat,
+  removeLastCat,
+  destructivelyRemoveFirstCat,
+  removeFirstCat
+};
